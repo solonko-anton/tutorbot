2aa4ec6f00cd5b24c3288ec5e10a72f489b2ce52
solonko-anton <asolonko42@gmail.com>
2025-06-29 19:16:19 +0000

    added tg id

--- app/models/user.py
+++ app/models/user.py
@@ -3,8 +3,10 @@ from sqlmodel import SQLModel, Field, Relationship
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    tg_id: int = Field()
     username: str = Field()
     first_name: str = Field()
     last_name: str = Field()
     phone: str = Field()
     patronymic: str = Field()
+    country: str = Field()
