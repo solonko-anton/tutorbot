0af095f2c58c931cd5847ac9cf5283e4b5cd4936
solonko-anton <asolonko42@gmail.com>
2025-07-04 08:25:26 +0000

    added easyocr, code refactored

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from aiogram.types import Update
 from app.config.config import settings
 from app.ui.user_router import user_router
 from app.utils.fsm.user_states import registration_router
+from app.utils.fsm.teacher_states import teacher_router
 from contextlib import asynccontextmanager
-import app.callbacks.enter_data
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     webhook_url = settings.get_webhook_url()
-    dp.include_routers(user_router,
-                       registration_router)
+    dp.include_routers(user_router, 
+                       registration_router,
+                       teacher_router)
     await bot.set_webhook(
         url=webhook_url,
         allowed_updates=None,
